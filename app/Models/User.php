f6486f42c008061cc8390f780cee45341122dc8c
Iván Cuerva <ivancuerva@gmail.com>
2021-10-05 13:28:47 +0200

    Datos de usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,16 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
+     * Lo creamos como constante para poder acceder a ellos
+     */
+    private const USERS = [
+        1 => ['id' => 1, 'email' => 'prueba1@ecommercefarm-prueba.com'],
+        2 => ['id' => 2, 'email' => 'prueba2@ecommercefarm-prueba.com'],
+        3 => ['id' => 3, 'email' => 'prueba3@ecommercefarm-prueba.com'],
+        4 => ['id' => 4, 'email' => 'prueba4@ecommercefarm-prueba.com'],
+    ];
+    
+    /**
      * The attributes that are mass assignable.
      *
      * @var string[]
@@ -41,4 +51,21 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    /**
+     * Método que dado un id devuelve la instacia de la clase user
+     * @param unknown $id
+     */
+    public static function searchUser ($id) {
+        //Comprobamos que exista el id, el cual coincide con la key del array
+        if (key_exists($id, self::USERS)) {
+            $user = new User();
+            $user->id = self::USERS[$id]['id'];
+            $user->email = self::USERS[$id]['email'];
+            
+            return $user;
+        }
+        
+        return null;
+    }
 }
